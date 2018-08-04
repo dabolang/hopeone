90813ef1b8fe5151ec87009b5e79daca115fde04
wilsonyou <40747875+wilsonyou@users.noreply.github.com>
2018-08-04 12:57:43 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -41,10 +41,10 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode', 
-    'sphinx_markdown_tables',
+    
    
 ]
-
+#'sphinx_markdown_tables',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
