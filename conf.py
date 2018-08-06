e8fa72e5ad08053f497c70f206ff72b68a615dc4
wilsonyou <40747875+wilsonyou@users.noreply.github.com>
2018-08-06 10:27:01 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,6 +17,16 @@
 
 import sys
 import os
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas','sphinx_markdown_tables']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
